d2bffb4f3f6c716f3b8641595d16ae08b08bebb5
Md-Mohimul-Alam <mohimreza1234@gmail.com>
2025-12-15 01:07:02 +0600

    changes

---
+++ next.config.ts
@@ -0,0 +1,38 @@
+import type {NextConfig} from 'next';
+
+const nextConfig: NextConfig = {
+  /* config options here */
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'placehold.co',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'i.ibb.co',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'www.transparenttextures.com',
+        port: '',
+        pathname: '/**',
+      }
+    ],
+  },
+  env: {
+    NEXT_PUBLIC_CLOUDINARY_CLOUD_NAME: 'dhbyx1flu',
+  }
+};
+
+export default nextConfig;
